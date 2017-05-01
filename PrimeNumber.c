904192bdea920bf6b2d8a15d136c1d93f161f664
DaejeonCitizen <rlatmdals9@naver.com>
2017-05-01 09:01:58 +0900

    Create PrimeNumber.c

---
+++ PrimeNumber.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void PrimeNumber(int count)
+{
+	int n1 = 2, n2, i;
+
+	for (i = 1; count >= i; n1++)
+	{
+		for (n2 = 2; n1 > n2; n2++)
+		{
+			if (n1%n2 == 0)
+				break;
+		}
+		if (n1 == n2)
+			printf("%d\n", n1), i++;
+	}
+}
+
+int main(void)
+{
+	int count;
+	printf("Number of PrimeNumber : ");
+	scanf("%d", &count);
+	PrimeNumber(count);
+}
