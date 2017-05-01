33df81d8f8e3e1434c5d4b2a0a514e692ebbd617
DaejeonCitizen <rlatmdals9@naver.com>
2017-05-01 17:25:45 +0900

    Create ArrangementMMS.c

---
+++ ArrangementMMS.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int num[5];
+	int i;
+	int max = 0, min = 0, sum = 0;
+
+	printf("input five values : ");
+	for (i = 0; i < 5; i++)
+		scanf("%d", &num[i]);
+
+	for (i = 0; i < 5; i++)
+		max = num[i] > max ? num[i] : max;
+	min = max;
+	for (i = 0; i < 5; i++)
+		min = num[i] < min ? num[i] : min;
+	for (i = 0; i < 5; i++)
+		sum += num[i];
+
+	printf("Max : %d\n", max);
+	printf("Min : %d\n", min);
+	printf("Sum : %d\n", sum);
+
+	return 0;
+}
