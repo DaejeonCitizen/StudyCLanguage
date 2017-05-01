635df837668ac72c9542a65ea6191785d2e96d66
DaejeonCitizen <rlatmdals9@naver.com>
2017-05-01 15:36:58 +0900

    Create SquareCompare.c

---
+++ SquareCompare.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int Square(int);
+
+int main(void)
+{
+	int num;
+	
+	printf("(2^k<=n)  n:");
+	scanf("%d", &num);
+
+	printf("2^%d<=%d", Square(num), num);
+
+	return 0;
+}
+
+int Square(int num)
+{
+	int sum = 1, i = 0;
+	while(1)
+	{
+		sum = sum * 2;
+
+		if (sum > num)
+			return i;
+
+		i++;
+		
+	}
+
+}
