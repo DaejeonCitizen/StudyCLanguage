29e114b168bcd3d0639924a7c5c14fdb1e48eecf
DaejeonCitizen <rlatmdals9@naver.com>
2017-05-28 00:17:40 +0900

    Create BaseBallGame.c

---
+++ BaseBallGame.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void StrikeBall(int pac[], int pau[], void (*Ball)(int c[], int u, int *ball))
+{
+	int i, s, b, c;
+	i = s = b = c = 0;
+
+	while (1)
+	{
+		printf("Select 3 number : \n");
+		scanf("%d %d %d", &pau[0], &pau[1], &pau[2]);
+
+		for (i = 0; i < 3; i++)
+		{
+			pac[i] == pau[i] ? s++ : Ball(pac, pau[i], &b);
+		}
+		c++;
+
+		printf("%d번째 도전 결과 : %dStrike, %dBall\n\n", c, s, b);
+		
+		if (s == 3)
+			break;
+		else
+			b = s = 0;
+
+	}
+	printf("Game over!\n");
+	
+}
+
+void Ball(int pac[], int pau, int * b)
+{
+	int i;
+
+	for (i = 0; i < 3; i++)
+		if (pau == pac[i])
+			*b++;
+	printf("%d", *b);
+}
+
+int main(void)
+{
+	int arrc[3] = { 0, };
+	int arru[3] = { 0, };
+	int i;
+
+	srand((int)time(NULL));
+
+	for (i = 0; i < 3; i++)
+		arrc[i] = rand() % 10;
+
+	printf("Game start!\n");
+
+	StrikeBall(arrc, arru, Ball);
+
+}
