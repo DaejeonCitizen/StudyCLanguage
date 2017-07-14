4e3255408840e881ac36e23a256eb3d9258d65ca
DaejeonCitizen <rlatmdals9@naver.com>
2017-07-14 14:08:26 +0900

    Update jobInfo.c

--- jobInfo.c
+++ jobInfo.c
@@ -2,7 +2,8 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define STR_JOB(A,B) #A "'s job is " #B
+#define STR_JOB(A,B) printf("%s's job is %s.\n",A,B);
+#define TEMPLEN 30
 
 typedef struct
 {
@@ -13,20 +14,34 @@ typedef struct
 void InputJobInfo(Jobinfo people[], int count)
 {
 	int i;
-	Jobinfo temp = { (char *)malloc(sizeof(char) * 30), (char *)malloc(sizeof(char) * 30) };
-
+	Jobinfo temp;
+	temp.name = (char *)malloc(sizeof(char) * 30);
+	temp.job = (char *)malloc(sizeof(char) * 30);
 	for (i = 0; i < count; i++)
 	{
-		printf("%d. Input name : ", i + 1), scanf_s("%s", temp.name,sizeof(temp.name));
+		printf("%d. Input name : ", i + 1);
+		scanf_s("%s", temp.name, TEMPLEN);
 		while (getchar() != '\n');
-		printf("%d. Input job : ", i + 1), scanf_s("%s", temp.job, sizeof(temp.job));
+		printf("%d. Input job : ", i + 1);
+		scanf_s("%s", temp.job, TEMPLEN);
 		while (getchar() != '\n');
 		people[i].name = (char *)malloc(sizeof(char) * strlen(temp.name) + 1);
 		people[i].job = (char *)malloc(sizeof(char) * strlen(temp.job) + 1);
-		strncpy_s(people[i].name, sizeof(temp.name) + 1, temp.name, sizeof(temp.name) + 1);
-		strncpy_s(people[i].job, sizeof(temp.job) + 1, temp.job, sizeof(temp.job) + 1);
+		strcpy_s(people[i].name, strlen(temp.name) + 1, temp.name);
+		strcpy_s(people[i].job, strlen(temp.job) + 1, temp.job);
 	}
-	
+	free(temp.name);
+	free(temp.job);
+}
+
+void UnassignMemory(Jobinfo * info, int count)
+{
+	int i;
+	for (i = 0; i < count; i++)
+	{
+		free(info[i].name);
+		free(info[i].job);
+	}
 }
 
 void ReadJobInfo(Jobinfo people[], int count)
@@ -35,7 +50,7 @@ void ReadJobInfo(Jobinfo people[], int count)
 
 	for (i = 0; i < count; i++)
 	{
-		printf("%s, %s\n", people[i].name,people[i].job);
+		STR_JOB(people[i].name, people[i].job);
 	}
 }
 int main(void)
@@ -43,10 +58,12 @@ int main(void)
 	Jobinfo * job;
 	int count;
 
-	printf("How many input to job of peaple? : "); scanf_s("%d", &count);
+	printf("How many input to job of peaple? : ");
+	scanf_s("%d", &count);
 	job = (Jobinfo *)malloc(sizeof(Jobinfo) * count);
 	InputJobInfo(job, count);
 	ReadJobInfo(job, count);
-
+	UnassignMemory(job, count);
+	
 	return 0;
 }
